3aa4f2e2762276dd1810dc10de2a37a333ce48ad
Chavan <SC035470@cerner.net>
2018-03-07 15:48:18 +0530

    My First Commit

---
+++ src/TestGit.java
@@ -0,0 +1,14 @@
+public class TestGit {
+	
+	public static void main(String args[]){
+        String apple[] = new String [5];
+        apple[0] = "Fresh";
+        apple[1] = "Fresh";
+        apple[2] = "Routen";
+        apple[3] = "Fresh";
+        apple[4] = "Routen";
+        
+        System.out.println("This Apple is"+ apple[4] +"not good to eat");
+        
+    }
+}
